c8350f7c265ea00c617c08dabb1aa9fe38001b51
D3stinn3 <paynedestinne@gmail.com>
2024-03-18 14:04:20 +0300

    More Contracts

---
+++ projects/ASA/smart_contracts/monkay/contract.py
@@ -0,0 +1,16 @@
+from puyapy import ARC4Contract, arc4
+
+
+class Monkay(ARC4Contract):
+
+    @arc4.abimethod()
+    def hello(self, name: arc4.String) -> arc4.String:
+        return "Hello, " + name
+
+    @arc4.abimethod()
+    def goodbye(self, name: arc4.String) -> arc4.String:
+        return "Goodbye, " + name
+
+    @arc4.abimethod()
+    def lottery(self, name: arc4.String, age: arc4.String) -> arc4.String:
+        return "Name, " + name + "Age, " + age
